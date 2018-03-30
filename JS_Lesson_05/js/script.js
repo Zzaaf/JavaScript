d216f03a350b852bfb984bed3467c310932c11d4
Alexej <radist-electric@yandex.ru>
2018-03-30 18:25:06 +0500

    Выполнил урок 5

---
+++ JS_Lesson_05/js/script.js
@@ -0,0 +1,37 @@
+let li = document.querySelector('.menu-item'),
+				newMenuItem = li.cloneNode(false),
+				ul = document.getElementsByTagName('ul')[0],
+				ulTest = document.body.getElementsByClassName('.menu'),
+				title = document.getElementById('title'),
+				adv = document.querySelector('.adv'),
+				column = document.querySelectorAll('.column');
+				answer = document.getElementById('prompt'),
+
+
+//В новый элемент списка добавляем текст
+newMenuItem.innerHTML = 'Пятый пункт';
+//Вставляем новый элемент списка в конец всего списка
+ul.appendChild(newMenuItem);
+//Меняем картинку заднего фона
+document.body.style.backgroundImage = 'url(img/apple_true.jpg)';
+//Меняем текст заголовка
+title.innerHTML = 'Мы продаем только подлинную технику Apple';
+//Удаляем рекламу
+column[1].removeChild(adv);
+
+//Задаём вопрос
+let question = prompt('Напишите Вашк отношение к технике Apple', '');
+	while (question == '' || question == null) {
+		question = prompt('Напишите Вашк отношение к технике Apple', '');
+	}
+	//И выводим ответ пользователя на экран
+answer.innerHTML = question;
+
+
+console.log(li);
+console.log(ul);
+console.log(ulTest);
+console.log(newMenuItem);
+console.log(title);
+console.log(column);
+console.log(adv);
